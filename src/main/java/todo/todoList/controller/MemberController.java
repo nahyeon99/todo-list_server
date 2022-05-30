83e8eda861f4a611f87793af324f4d462c45d0ce
NaHyeon <nahyeonee99@gmail.com>
2022-05-30 14:08:57 +0900

    Feat: controller(Member, ToDo) 개발

---
+++ src/main/java/todo/todoList/controller/MemberController.java
@@ -0,0 +1,160 @@
+package todo.todoList.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import todo.todoList.domain.Member;
+import todo.todoList.domain.ToDo;
+import todo.todoList.service.MemberService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+public class MemberController {
+
+    private MemberService memberService;
+
+    /**
+     * 회원 생성 기능
+     */
+    @PostMapping("/members")
+    public CreateMemberResponse createMember(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        member.setAge(request.getAge());
+        member.setEmail(request.getEmail());
+        member.setCreatedAt(LocalDateTime.now());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 회원 수정 기능
+     */
+    @PatchMapping("/members/{id}")
+    public UpdateMemberResponse updateMember(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request
+    ) {
+        memberService.update(id, request.getAge(), request.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName(), findMember.getAge());
+    }
+
+    /**
+     * 회원 리스트 조회 기능 - 회원만
+     */
+    @GetMapping("/members")
+    public Result findMemberAll() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberToDo> collect = findMembers.stream()
+                .map(m -> new MemberToDo(m))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+    @GetMapping("/membners/{id}")
+    public MemberToDo findMemberOne(@PathVariable("id") Long id) {
+        Member member = memberService.findOne(id);
+        return new MemberToDo(member);
+    }
+
+
+    /**
+     * DTO
+     */
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+        @NotEmpty
+        private String email;
+        private Integer age;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    class UpdateMemberRequest {
+        @NotEmpty
+        private String name;
+        private Integer age;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    class UpdateMemberResponse {
+        private Long id;
+        private String name;
+        private Integer age;
+    }
+
+    @Data
+    @AllArgsConstructor // 이건 왜 사용했을까?
+    class MemberToDo {
+        private Long id;
+        private String name;
+        private Integer age;
+        private String email;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private List<TodoListDto> todoList;
+
+        public MemberToDo(Member member) {
+            id = member.getId();
+            age = member.getAge();
+            email = member.getEmail();
+            createdAt = member.getCreatedAt();
+            updatedAt = member.getUpdatedAt();
+            name = member.getName();
+            todoList = member.getToDoList().stream()
+                    .map(todo -> new TodoListDto(todo))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class TodoListDto{
+        private Long todoId;
+        private String content;
+        private Boolean isCompleted;
+        private LocalDateTime createAt;
+        private LocalDateTime updateAt;
+        public TodoListDto(ToDo todo){
+            content=todo.getContent();
+            todoId=todo.getId();
+            isCompleted=todo.getIsCompleted();
+            createAt=todo.getCreatedAt();
+            updateAt=todo.getUpdatedAt();
+        }
+    }
+}
---
+++ src/main/java/todo/todoList/controller/ToDoController.java
@@ -0,0 +1,166 @@
+package todo.todoList.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import todo.todoList.domain.ToDo;
+import todo.todoList.service.MemberService;
+import todo.todoList.service.ToDoService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+// @Controller + @ResponseBody // Rest API 스타일
+// @ResponseBody 데이터 자체를 json 이나 xml로 보내자
+@RestController
+@RequiredArgsConstructor
+public class ToDoController {
+
+    private final ToDoService toDoService;
+    private final MemberService memberService;
+
+
+    /**
+     * todo 생성 기능
+     */
+    @PostMapping("/members/{memberId}/todos")
+    public CreateToDoResponse createToDo(
+            @PathVariable("memberId") Long memberId, @RequestBody @Valid CreateToDoRequest request) {
+        ToDo toDo = ToDo.createToDo(memberService.findOne(memberId), request.content);
+        Long id = toDoService.join(toDo);
+        return new CreateToDoResponse(id);
+    }
+
+    /**
+     * todo 수정 기능 - 완료 여부만 변경
+     */
+    @PatchMapping("/todos/{todoid}")
+    public UpdateToDoResponse updateToDoResponse(
+            @PathVariable("todoid") Long id,
+            @RequestBody @Valid UpdateToDoRequest request
+    ) {
+        toDoService.update(id, request.getIsCompleted());
+        ToDo findToDo = toDoService.findOne(id);
+        return new UpdateToDoResponse(findToDo.getId(), findToDo.getIsCompleted());
+    }
+
+    /**
+     * todo 삭제 기능
+     */
+    @DeleteMapping("/todos/{todoid}")
+    public DeleteToDoResponse deleteToDoResponse(
+            @PathVariable("todoid") Long id
+    ) {
+        toDoService.delete(id);
+        return new DeleteToDoResponse(id);
+    }
+
+    /**
+     * todo 를 식별자를 이용하여 조회 기능 (회원까지 같이 조회 다대일)
+     */
+    @GetMapping("/todos/{todoid}")
+    public GetToDoResponse getToDoResponse(
+            @PathVariable("todoid") Long id
+    ) {
+        ToDo findToDo = toDoService.findOne(id);
+        return new GetToDoResponse(findToDo);
+    }
+
+    /**
+     * DTO
+     */
+    @Data
+    static class GetToDoResponse {
+        private String name;
+        private String content;
+        private Long todoId;
+        private Long memberId;
+        private Boolean isCompleted;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+        public GetToDoResponse(ToDo todo) {
+            content = todo.getContent();
+            todoId=todo.getId();
+            memberId=todo.getMember().getId();
+            name=todo.getMember().getName();
+            isCompleted=todo.getIsCompleted();
+            createdAt = todo.getCreatedAt();
+            updatedAt=todo.getUpdatedAt();
+        }
+    }
+
+    /**
+     * TODO 생성 - 응답 : 상태코드만
+     */
+
+    @Data
+    static class CreateToDoRequest {
+        private Long memberId;
+        private String content;
+    }
+
+    @Data
+    static class CreateToDoResponse {
+        private Long id;
+
+        public CreateToDoResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * TODO 수정 - 응답 : 상태코드만
+     */
+    @Data
+    @AllArgsConstructor
+    static class UpdateToDoRequest {
+        private Boolean isCompleted;
+        public UpdateToDoRequest() {
+
+        }
+    }
+
+    /**
+     * TODO 수정 - 응답 : 상태코드만
+     */
+    @Data
+    @AllArgsConstructor
+    static class UpdateToDoResponse {
+        private Long id;
+        private Boolean isCompleted;
+    }
+
+    /**
+     * TODO 삭제 - 응답 : 상태코드만
+     */
+    @Data
+    @AllArgsConstructor
+    static class DeleteToDoResponse {
+        private Long id;
+    }
+
+    @Data
+    static class ToDoDto {
+        private Long toDoId;
+        private String name;
+        private String content;
+        private Boolean isCompleted;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private Long memberId;
+
+        public ToDoDto(ToDo toDo) {
+            toDoId = toDo.getId();
+            name = toDo.getMember().getName();
+            content = toDo.getContent();
+            createdAt = toDo.getCreatedAt();
+            isCompleted = toDo.getIsCompleted();
+            memberId = toDo.getMember().getId();
+        }
+
+
+    }
+
+}
